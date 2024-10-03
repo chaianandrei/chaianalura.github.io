0130babb4dca63d137bdfa965263df3a8bb5f301
chaianandrei <129392644+chaianandrei@users.noreply.github.com>
2024-10-03 07:58:04 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,12 +3,30 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const paragrafo = document.createElement('p')
     const paragrafo document.createElement('p')
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
 console.log(paragrafo)
    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(paragrafo)
+    // código omitido
+const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.tempo_medio)
+const minutos = (dados.tempo_medio - horas) * 100
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+
+// código omitido
 
 const container = document.getElementById('graficos-container')
 container.appendChild(paragrafo)
